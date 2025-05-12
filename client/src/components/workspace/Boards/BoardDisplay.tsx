d6ceb782aabc544f63f0d5fb72257f38a6b7c752
Zynalex9 <alzn952@gmail.com>
2025-05-12 17:51:37 +0500

    Aspect ratio fix

--- client/src/components/workspace/Boards/BoardDisplay.tsx
+++ client/src/components/workspace/Boards/BoardDisplay.tsx
@@ -18,31 +18,32 @@ const BoardDisplay = () => {
 
   if(loading) return <h1 className="text-center text-textP text-4xl font-charlie-text-sb">Loading boards...</h1>
   return (
-    <div className="py-8 flex flex-wrap items-center justify-center gap-2">
-      <div className="bg-[#333C43] text-center font-charlie-text-sb text-textP text-2xl p-12 w-[30%] rounded-xl shadow-2xl transition-colors duration-150 hover:bg-[#333C43]/50">
-        <h2>Create new board</h2>
-      </div>
-      {boards?.yourBoards.map((board) => (
-        <Link
-          to=" "
-          key={board._id}
-          style={
-            isImageUrl(board.cover)
-              ? {
-                  backgroundImage: `url(${board.cover})`,
-                  backgroundSize: "cover",
-                  backgroundPosition: "center",
-                }
-              : { backgroundColor: board.cover }
-          }
-          className="p-12 w-[30%] rounded-xl shadow-2xl transition-transform duration-200 hover:scale-95"
-        >
-          <h1 className="text-textP font-bold font-charlie-text-sb text-2xl text-center">
-            {board.title}
-          </h1>
-        </Link>
-      ))}
-    </div>
+   <div className="py-8 flex flex-wrap items-center justify-center gap-4">
+  <div className="bg-[#333C43] aspect-video flex items-center justify-center text-center font-charlie-text-sb text-textP text-2xl w-[30%] rounded-xl shadow-2xl transition-colors duration-150 hover:bg-[#333C43]/50">
+    <h2>Create new board</h2>
+  </div>
+  {boards?.yourBoards.map((board) => (
+    <Link
+      to=" "
+      key={board._id}
+      style={
+        isImageUrl(board.cover)
+          ? {
+              backgroundImage: `url(${board.cover})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }
+          : { backgroundColor: board.cover }
+      }
+      className="aspect-video flex items-center justify-center w-[30%] rounded-xl shadow-2xl transition-transform duration-200 hover:scale-95"
+    >
+      <h1 className="text-textP font-bold font-charlie-text-sb text-2xl text-center">
+        {board.title}
+      </h1>
+    </Link>
+  ))}
+</div>
+
   );
 };
 
