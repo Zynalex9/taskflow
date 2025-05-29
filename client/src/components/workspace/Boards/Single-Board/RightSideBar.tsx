ba9d260086e95e46e70ab728e8d67fd71886bc67
Zynalex9 <alzn952@gmail.com>
2025-05-29 10:37:33 +0500

    feat: Enhance RightSideBar component; implement keyboard navigation and improve sidebar visibility

--- client/src/components/workspace/Boards/Single-Board/RightSideBar.tsx
+++ client/src/components/workspace/Boards/Single-Board/RightSideBar.tsx
@@ -1,13 +1,30 @@
+import { useEffect } from "react";
+
 interface IProps {
   setOpenSideBar: React.Dispatch<React.SetStateAction<boolean>>;
-  openSidebar:boolean
+  openSidebar: boolean;
 }
 
 const RightSideBar = ({ openSidebar, setOpenSideBar }: IProps) => {
+useEffect(() => {
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.code === "BracketRight" || e.key === "]") {
+      e.preventDefault();
+      setOpenSideBar((prev) => !prev);
+    }
+  };
+
+  document.addEventListener("keydown", handleKeyDown);
+  
+  return () => {
+    document.removeEventListener("keydown", handleKeyDown);
+  };
+}, []);
   return (
     <div
-      className={`fixed right-0 top-0 transition-transform duration-300 ease-in-out transform bg-[#282E33] w-[20rem] h-[89vh] z-50 ${
-        openSidebar ? 'translate-x-0' : 'translate-x-full'
+      onClick={() => setOpenSideBar(false)}
+      className={`fixed right-0 top-0 transition-opacity duration-300 ease-in-out transform bg-[#282E33] w-[20rem] h-[89.8vh] z-50  overflow-y-auto ${
+        openSidebar ? "opacity-100" : "opacity-0 pointer-events-none"
       }`}
     ></div>
   );
