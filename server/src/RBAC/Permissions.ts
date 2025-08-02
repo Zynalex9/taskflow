c8032ed1488963a121d42eea6a5927950ba3aeaa
Zynalex9 <alzn952@gmail.com>
2025-08-02 21:56:00 +0500

    feat: enhance permissions handling by adding list permissions and updating board permissions retrieval

--- server/src/RBAC/Permissions.ts
+++ server/src/RBAC/Permissions.ts
@@ -13,6 +13,12 @@ export const PERMISSIONS = {
   BOARD_MANAGE_ADMINS: "board:manage_admins",
   BOARD_CREATE_LIST: "board:create_list",
 
+  LIST_VIEW: "list:view",
+  LIST_EDIT: "list:edit",
+  LIST_DELETE: "list:delete",
+  LIST_MANAGE_MEMBERS: "list:manage_members",
+  LIST_CREATE_CARD: "list:create_card",
+
   CARD_VIEW: "card:view",
   CARD_EDIT: "card:edit",
   CARD_DELETE: "card:delete",
--- server/src/RBAC/getPermissions.ts
+++ server/src/RBAC/getPermissions.ts
@@ -1,6 +1,7 @@
 import { Types } from "mongoose";
 import { workSpaceModel } from "../models/workspace.model";
 import { ROLES } from "./Roles";
+import { boardModel } from "../models/board.models";
 
 export const getWorkspacePermissions = async (
   userId: Types.ObjectId,
@@ -28,10 +29,9 @@ export const getBoardPermissions = async (
   userId: Types.ObjectId,
   boardId: string
 ): Promise<string[]> => {
-  const board = await workSpaceModel.findById(boardId);
+  const board = await boardModel.findById(boardId);
   if (!board) return [];
   if (board.createdBy.equals(userId)) return ROLES.BOARD_ADMIN.permissions;
-  if (board.admin.includes(userId)) return ROLES.BOARD_ADMIN.permissions;
 
   const member = board.members.find((m) => m.user.equals(userId));
   if (member) {
@@ -41,6 +41,48 @@ export const getBoardPermissions = async (
       return ROLES.BOARD_MEMBER.permissions;
     }
   }
-
+  if (board.visibility === "private") {
+    return [];
+  } else if (board.visibility === "workspace") {
+    const workspace = await workSpaceModel.findById(board.workspace);
+    if (workspace) {
+      const member = workspace.members.find((m) => m.user.equals(userId));
+      if (member) {
+        if (member.role === "admin") {
+          return ROLES.BOARD_ADMIN.permissions;
+        } else {
+          return ROLES.BOARD_MEMBER.permissions;
+        }
+      }
+    }
+  }
   return [];
+};
+
+export const getPermission = async (
+  userId: Types.ObjectId,
+  resourceType: "workspace" | "board" | "list" | "card",
+  resourceId: string
+): Promise<string[]> => {
+  const permissions = new Set<string>();
+  if (resourceType === "workspace") {
+    const workspacePermissions = await getWorkspacePermissions(
+      userId,
+      resourceId
+    );
+    workspacePermissions.forEach((p) => permissions.add(p));
+  }
+  if (resourceType === "board") {
+    const boardPermissions = await getBoardPermissions(userId, resourceId);
+    boardPermissions.forEach((p) => permissions.add(p));
+    const board = await boardModel.findById(resourceId);
+    if (board?.workspace) {
+      const workspacePermissions = await getWorkspacePermissions(
+        userId,
+        board?.workspace.toString()
+      );
+      workspacePermissions.forEach((p) => permissions.add(p));
+    }
+  }
+  return Array.from(permissions);
 };
